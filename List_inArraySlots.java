bbf03219fbe26b80a6f5951ae48b25f32ea03812
Amy Lei <42757189+amy-lei@users.noreply.github.com>
2019-03-06 14:45:25 -0500

    Added comments for perspicuity
    Reflects the changes made during class time

--- List_inArraySlots.java
+++ List_inArraySlots.java
@@ -1,36 +1,50 @@
 public class List_inArraySlots{
 	
+	//FIELDS
 	private int[] refArray;
-	private int filledElements;
+	private int filledElements; 
 
 	public List_inArraySlots(){
 		this.refArray = new int[10];
 		filledElements = 0;
 	}
 
-	public List_inArraySlots(int[] a){
-		this.refArray = a;
-		filledElements = a.length;
+	// ===========================================================
+	// CONSTRUCTORS
+	
+	/* 
+	To instantiate an array according to an existing array
+	precondition: existing array does not have null elements
+	*/
+	public List_inArraySlots(int[] existingArray){
+		this.refArray = existingArray;
+		filledElements = existingArray.length;
 	}
+	
+	
+	// ===========================================================
+	// METHODS
 	
 	public int size(){
 		return filledElements;
-	}
-    /** 
+  	}
+	
+      /** 
        @return a string representation of this list,
        in [a,b,c,] format
-      */ 
+       */ 
      	public String toString() {
      	    	String ans = "[";
-		for(int index=0;index<filledElements;index++){
+		for(int index=0; index<filledElements; index++){
 			ans += refArray[index] + ",";
 		}
+		
 		ans += "]";
 		return ans;
      	}
 
     
-    /** 
+     /** 
       Appends @value to the end of this list.
       
       @return true, in keeping with conventions yet to be discussed
@@ -42,17 +56,18 @@ public class List_inArraySlots{
       }
 
 
-    /** 
+     /** 
       Double the capacity of the List_inArraySlots, 
       preserving existing data
      */
       private void expand() {
          System.out.println( "expand... (for debugging)");
-         int[] extended = new int[filledElements * 2];
-	 for(int index=0; index < filledElements;index++){
+	      
+         int[] extended = new int[refArray.length * 2];         // instantiate an array with twice the capacity
+	 for(int index=0; index < filledElements;index++){      // copy over each non-null value
 		extended[index] = refArray[index];
 	 }
-	 refArray = extended;
+	 refArray = extended;					// replace the reference held in refArray to the new array
 		
       }
 }
