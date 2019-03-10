11ba277c553cb9e03892fea0583fc9cb5f8eac77
Amy Lei <amy.lei2001@yahoo.com>
2019-03-10 18:29:15 -0400

    updated to reflect hw50

--- List_inArraySlots.java
+++ List_inArraySlots.java
@@ -1,84 +1,172 @@
 public class List_inArraySlots{
-	// ===========================================================
-	// FIELDS
-	private int[] refArray;
-	private int filledElements;
+    // ===========================================================
+    // FIELDS
+    private int[] refArray;
+    private int filledElements;
 
-	private static final int INITIAL_CAPACITY = 10; 
+    private static final int INITIAL_CAPACITY = 10;
 
-	public List_inArraySlots(){
-		this.refArray = new int[INITIAL_CAPACITY];
-		filledElements = 0;
-	}
+    public List_inArraySlots(){
+        this.refArray = new int[INITIAL_CAPACITY];
+        filledElements = 0;
+    }
 
-	// ===========================================================
-	// CONSTRUCTORS
-	
-	/* 
-	To instantiate an array according to an existing array
-	precondition: existing array does not have null elements
-	*/
-	public List_inArraySlots(int[] existingArray){
-		this.refArray = existingArray;
-		filledElements = existingArray.length;
-	}
-	
-	
-	// ===========================================================
-	// METHODS
-	
-	public int size(){
-		return filledElements;
-  	}
+    // ===========================================================
+    // CONSTRUCTORS
 
-	public int getValue(int index){
-		return refArray[index];
-	}
-	
-        /** 
-        @return a string representation of this list,
-        in [a,b,c,] format
-        */ 
-        public String toString() {
-     	    String ans = "[";
-		for(int index = 0; index<filledElements; index++){
-			ans += refArray[index] + ",";
-		}
-		
-		ans += "]";
-		return ans;
-     	    }
-
-    
-        /** 
-        Appends @value to the end of this list.
-      
-        @return true, in keeping with conventions yet to be discussed
-        */
-        public boolean add( int value) {
-            // first check if the list is already full
-            if (refArray.length == filledElements)
-                expand(); // if so, expand the array
-    
-            // then add the value 
-            refArray[filledElements] = value;
-            filledElements ++;
-        
-            return true;
-       }
+    /*
+    To instantiate an array according to an existing array
+    precondition: existing array does not have null elements
+    */
+    public List_inArraySlots(int[] existingArray){
+        this.refArray = existingArray;
+        filledElements = existingArray.length;
+    }
 
 
-    /** 
-    Double the capacity of the List_inArraySlots, 
+    // ===========================================================
+    // METHODS from v0
+
+    public int size(){
+    return filledElements;
+    }
+
+    /**
+    @return a string representation of this list,
+    in [a,b,c,] format
+    */
+    public String toString() {
+        String ans = "[";
+        for(int index = 0; index<filledElements; index++){
+            ans += refArray[index] + ",";
+        }
+
+        ans += "]";
+        return ans;
+    }
+
+
+    /**
+    Appends @value to the end of this list.
+
+    @return true, in keeping with conventions yet to be discussed
+    */
+    public boolean add( int value) {
+        // first check if the list is already full
+        if (refArray.length == filledElements)
+            expand(); // if so, expand the array
+
+        // then add the value
+        refArray[filledElements] = value;
+        filledElements ++;
+
+        return true;
+    }
+
+
+    /**
+    Double the capacity of the List_inArraySlots,
     preserving existing data
     */
     private void expand() {
-
+        
         int[] extended = new int[refArray.length * 2];         // instantiate an array with twice the capacity
         for(int index=0; index < filledElements;index++){      // copy over each non-null value
             extended[index] = refArray[index];
         }
         refArray = extended;                                   // replace the reference held in refArray to the new array
-        
+
       }
+
+    // ===========================================================
+    // METHODS from v1
+
+    /**
+    accessor
+    @return element @index from this list
+    precondition: @index is within the bounds of the array.
+    (Having warned the user about this precondition,
+    you should NOT complicate your code to check
+    whether user violated the condition.)
+     */
+    public int get( int index ) {
+        return refArray[index];
+    }
+
+    /**
+    Store et value at @index to @newValue
+
+    @return old value at @index
+    @precondition: @index is within the bounds of this list.
+    */
+    public int set( int index, int newValue ) {
+        // store old value in local variable to return later
+        int oldValue = refArray[index];
+
+        // assign new value
+        refArray[index] = newValue;
+
+        return oldValue;
+    }
+    
+    
+    /**
+      Remove the element at position @index in this list.
+      Shift any subsequent elements to the left (that is,
+      decrease the index associated with each).
+
+      @return the value that was removed from the list
+     */
+     public int remove( int index) {
+        int removed = refArray[index];
+
+        for( ; index < filledElements; index++){
+            // set the element to the value of the element to the left
+            refArray[index] = refArray[index + 1];
+        }
+        
+        // update number of elements
+        filledElements--;
+        
+        return removed;
+     }
+
+
+    /**
+    Insert @value at position @index in this list.
+
+    Shift the element currently at that position (if any)
+    and any subsequent elements to the right
+    (that is, increase the index associated with each).
+    */
+    public void add( int index, int value) {
+
+        // keep track of element being moved and element that will be replaced
+        int elemReplaced = 0;
+        int elemShifting = refArray[index];
+        
+        //expand if necessary
+        if (filledElements + 1 > refArray.length) expand();
+              
+        // set the new value to given index
+        refArray[index] = value;   
+
+        // update filledElements 
+        filledElements++;
+
+        // shift the rest of the elements by...
+        for ( ++index; index < filledElements; index++){
+            //  keep track of the element that will be replaced
+            elemReplaced = refArray[index];
+
+            // now replace with the element you were shifting
+            refArray[index] = elemShifting;
+
+            //update the element you are elemShifting
+            elemShifting = elemReplaced;
+        }
+        
+        
+    }
+
 }
--- UserOfList.java
+++ UserOfList.java
@@ -1,9 +1,12 @@
-/** 
+/**
  Test list features.
  */
 public class UserOfList {
+    
+    private static List_inArraySlots list;
+    
     public static void main( String[] args ) {
-        List_inArraySlots list = new List_inArraySlots();
+        list = new List_inArraySlots();
 
          System.out.println( "number of elements: " + list.size() );
          System.out.println( "empty list:" + list);
@@ -18,22 +21,108 @@ public class UserOfList {
         }
          System.out.println("initial population of " + list.size() + " elements:");
          System.out.println( list);
-        
+
         // Add enough elements that expansion is expected
         for( ; i < 15; i++ ) {
-            
+
             if( i == 10) System.out.println( "expansion expected");
-            
+
             list.add( -i);
             System.out.println( "number of elements: " + list.size() );
         }
         System.out.println("result of second population: " + list.size() + " elements:");
         System.out.println( list);
-        
+
         // Trust no one.
-        for( ; i < 35; i++ ) 
+        for( ; i < 35; i++ )
             list.add( -i);
         System.out.println("after second expansion: " + list.size() + " elements:");
         System.out.println( list);
+
+        // --------- end of "code that worked in v0" ---------
+
+        
+        // test accessor
+        System.out.println( "sample elements from list:");
+        for( int elemIndex = 1; elemIndex < list.size(); elemIndex *= 2 ) {
+            System.out.println( "element " + elemIndex + ": "
+                              + list.get( elemIndex)
+                              );
+        }
+
+
+        // test set
+        setTest(  8);
+        setTest( 16);
+
+        // test remove
+        System.out.println( "remove " 
+                            + list.remove(8)
+                            + ", resulting in "
+                            + list.size()
+                            + " elements:\n"
+                            + list
+                            + System.lineSeparator()
+                          );
+
+
+        // test add
+        addAtTest( 0, 29); // beginning of the list
+        System.out.println();
+
+        addAtTest( list.size(), 17); // end of the list using the new add method
+        System.out.println();
+
+        addAtTest( 2, 19); // middle of a small list
+        System.out.println();
+
+        addAtTest( 2, 23); // not quite
+        System.out.println();
+        
+        addAtTest( 0, 0); // almost there
+        System.out.println();
+        
+        addAtTest( 0, 10); // filled the list
+        System.out.println();
+
+        addAtTest( 0, 100); // force an expansion or you get an error
+        System.out.println();
+
+
+    }
+
+
+    /**
+      Test the set() method, reporting and
+      changing the value at index @modifyAt.
+     */
+     private static void setTest( int modifyAt ) {
+        System.out.println(
+            "changed element " + modifyAt + " from "
+          + list.set( modifyAt, modifyAt + 1000) + " to "
+          + list.get( modifyAt)
+          );
+        }
+
+    /**
+    Test the 2-argument add( index, value) method.
+    */
+    private static void addAtTest( int addAt
+                             , int value
+                             ) {
+        System.out.print( "insert " 
+                        + value
+                        + " at position " 
+                        + addAt);
+                        
+        list.add(addAt, value);
+        
+        System.out.print(", resulting in "  
+                        + list.size() 
+                        + " elements:"
+                        + System.lineSeparator()
+                        + list
+                        + System.lineSeparator()
+        );
     }
 }
